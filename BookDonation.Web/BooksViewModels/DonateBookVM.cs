12eda59ad1d7e96b694103ebe27f80487a490996
Ismael Ouedraogo <ismael44131@outlook.com>
2018-11-13 10:43:12 -0500

    D

---
+++ BookDonation.Web/BooksViewModels/DonateBookVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookDonation.Web.BooksViewModels
+{
+    public class DonateBookVM
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3,
+            ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+        //[Required] - not required so they can add inventory by only putting in the SKU
+        [StringLength(200, MinimumLength = 5,
+            ErrorMessage = "Author name is required")]
+        public string Author { get; set; }
+        [Range(0, int.MaxValue,
+            ErrorMessage = "ISBN is required")]
+        public string ISBN { get; set; }
+        [Range(0, int.MaxValue,
+            ErrorMessage = "Please enter a Quantity value 0 or greater")]
+        public int QtyAvailable { get; set; }
+
+        public IEnumerable<SelectListItem> Genre
+        {
+            get
+            {
+                return new[]
+                    {
+                     new SelectListItem { Value = "", Text = "" },
+                     new SelectListItem { Value = "Fiction", Text = "Fiction" },
+                     new SelectListItem { Value = "Non-Fiction", Text = "Non-Fiction" },
+                     new SelectListItem { Value = "Children", Text = "Children" },
+                };
+            }
+        }
+    }
+}
---
+++ BookDonation.Web/BooksViewModels/ReserveBookVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookDonation.Web.BooksViewModels
+{
+    public class ReserveBookVM
+    {
+    }
+}
