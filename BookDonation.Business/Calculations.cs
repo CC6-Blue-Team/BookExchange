fb3504e1d2f8ca17fe7429ee300e631b0692ce29
Paula Holubek <paulaholubek@att.net>
2018-11-13 14:23:46 -0500

    QtyAvail add or minus code addition

---
+++ BookDonation.Business/Calculations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookDonation.Business
+{
+    public class Calculations
+    {
+        public byte AddOne(byte qtyAvail)
+        {
+            int TotAvail = Convert.ToInt32(qtyAvail) + 1;
+            return qtyAvail = Convert.ToByte(TotAvail);
+        }
+
+        public byte MinusOne(byte qtyAvail)
+        {
+            int TotAvail = Convert.ToInt32(qtyAvail) - 1;
+            return qtyAvail = Convert.ToByte(TotAvail);
+        }
+    }
+}
